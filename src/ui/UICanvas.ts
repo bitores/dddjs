9d5d559d21811cbb2491c3e05177650947ccb446
huangzj <huangzj@huangzj.mzftech.cn>
2018-10-18 19:38:24 +0800

    fix canvas

--- src/ui/UICanvas.ts
+++ src/ui/UICanvas.ts
@@ -5,14 +5,26 @@ export class UICanvas extends Base {
   constructor(public canvas: HTMLCanvasElement) {
     super()
     this.ctx = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
-
-
     if (this.ctx === null) {
       throw new Error('your browser not support the webgl .')
+    } else {
+      // this.width = this.ctx.
     }
 
-    return this;
+    this.toString()
+    this._init2()
   }
+
+  _init2() {
+    this.ctx = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
+    if (this.ctx === null) {
+      throw new Error('your browser not support the webgl .')
+    } else {
+      // this.width = this.ctx.
+    }
+  }
+
+
   get className() {
     return 'Node';
   }
