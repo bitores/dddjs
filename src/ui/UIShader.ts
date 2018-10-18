6404d09df0fb3cc71f66e1bdd1aa00a4672f313a
杨会敏 <yanghuimin@192.168.8.108>
2018-10-19 00:59:01 +0800

    add shader class

--- src/ui/UIShader.ts
+++ src/ui/UIShader.ts
@@ -4,18 +4,30 @@ export class UIShader extends Base {
   vertShader: WebGLShader;
   fragShader: WebGLShader;
   program: WebGLProgram;
-  vars: Object = {};
+  vars: Object = {
+    attribute: {},
+    uniform: {}
+  };
   // private qq: RegExp = /\s+/g;
+  // /(attribute|uniform)\s\S+\s\S+;/g
   constructor(public ctx: WebGLRenderingContext, public vertSource: string, public fragSource: string, public name: string = '') {
     super()
     this.vertShader = this.compileShader(vertSource, ctx.VERTEX_SHADER);
     this.fragShader = this.compileShader(fragSource, ctx.FRAGMENT_SHADER);
     this.program = this.linkProgram(this.vertShader, this.fragShader)
-    this.analySource();
+    this.analySource(vertSource);
+    this.analySource(fragSource);
+    console.log(this.vars)
   }
 
-  analySource() {
-
+  analySource(source: string) {
+    let format = source.replace(/\s+/g, ' ');
+    let matchs = format.match(/(attribute|uniform)\s\S+\s\S+;/g);
+    matchs && matchs.forEach(record => {
+      record = record.replace(';', '');
+      let ret = record.split(' ');
+      this.vars[ret[0]][ret[2]] = ret[1];
+    })
   }
 
   private compileShader(source: string, shaderType: number) {
